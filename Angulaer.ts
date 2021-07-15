63a0566b87c180e1645fa2541244992f3718f022
Farhad-009 <61062795+Farhad-00009@users.noreply.github.com>
2021-07-15 22:04:02 +0600

    Add files via upload

--- Angulaer.ts
+++ Angulaer.ts
@@ -1,8 +1,10 @@
-<body ng-app="ngAnimate">
 
-Hide the DIV: <input type="checkbox" ng-model="myCheck">
+<body ng-app="myApp">
+
+<h1>Hide the DIV: <input type="checkbox" ng-model="myCheck"></h1>
 
 <div ng-hide="myCheck"></div>
 
-</body>
-
+<script>
+var app = angular.module('myApp', ['ngAnimate']);
+</script> 
